2c1120fc7fb54dc4effd20b1446d7336bed5ee1d
Matias Stephens <mstephens@riafinancial.com>
2022-05-15 22:54:33 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<section class="align">
+    <h1>Random gif cat</h1>
+    <form @submit.prevent="crearGif()">
+        <div class="input">
+            <label for="title">Título</label>
+            <input class="input-value" type="text" v-model="titulo" required>
+        </div>
+        <div class="input">
+            <label for="filter">Filtro</label>
+            <select class="input-value" name="filter" v-model="filtro" required>
+                <option v-for="(filtro, $index) in filtros" :key="$index" :value="filtro"> {{ filtro }}</option>
+            </select>
+        </div>
+        <div class="input">
+            <label for="color">Color</label>
+            <select class="input-value" name="color" v-model="color" required>
+                <option v-for="(color, $index) in colores" :key="$index" :value="color">{{ color }}</option>
+            </select>
+        </div>
+        <div class="input">
+            <label for="tamaño">Tamaño</label>
+            <input class="input-value" type="text" v-model="tamaño" required>
+        </div>
+        <button type="submit">Obtener mi gato</button>
+    </form>
+    <div class="image">
+        <img v-if="url" :src="url" alt="gif gato">
+    </div>
+</section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    data: () => ({
+        url: "",
+        titulo: "",
+        filtro: "blur",
+        filtros: ["blur", "mono", "sepia", "negative", "paint", "pixel"],
+        color: "red",
+        colores: ["red", "blue", "green", "white", "yellow"],
+        tamaño: ""
+    }),
+    methods: {
+        crearGif() {
+            this.url = `https://cataas.com/cat/says/${this.titulo}?size=${this.tamaño}&color=${this.color}&filter=${this.filtro}`
+        }
+    }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+button {
+    height: 32px;
+    margin-top: 8px;
 }
+.input-value {
+    height: 32px;
+    border-width: 1px;
+    border-radius: 5px;
+    width: 250px;
+    padding: 0px;
+}
+.input {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 16px;
+    margin: 8px;
+    justify-content: space-between;
+    
+}
+img {
+    margin: 12px;
+}
+.align {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
 </style>
